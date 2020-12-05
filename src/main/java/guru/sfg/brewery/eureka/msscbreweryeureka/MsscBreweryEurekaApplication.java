240299991f72b8b6e4aac1fbccf34d066cdb1fd0
Harish krishnan <harishkrishnan1993@gmail.com>
2020-12-05 21:56:35 +0530

    Eureka config

--- src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
@@ -2,7 +2,9 @@ package guru.sfg.brewery.eureka.msscbreweryeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MsscBreweryEurekaApplication {
 
